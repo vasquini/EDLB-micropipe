127e01c9dbb0b6731b0af4879bcf555a10b29c4a
vasquini <suj7@cdc.gov>
2024-03-22 15:17:26 -0400

    Changed the version of Dorado

--- EDLB/DoradoBasecalling/run_dorado_basecaller.sh
+++ EDLB/DoradoBasecalling/run_dorado_basecaller.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 source /etc/profile 
 # Assign Job-Name instead of defauly which is the name of job-script
-#$ -N nfdorado
+#$ -N v0.5.1
 # Start the script in the current working directory
 #$ -V -cwd
 # Send to GPU queue (w/out gpu use all.q and uncomment gpu stuff)
@@ -13,19 +13,17 @@ source /etc/profile
 #$ -l h_rt=24:00:00
 #$ -l h_vmem=200G
 # Specify where standard output and error are stored.
-#$ -o dorado.out
-#$ -e dorado.err
+#$ -o v0.5.1.out
+#$ -e v0.5.1.err
 
-# ml guppy
-# which guppy_basecaller
-# which guppy_barcoder
 ml nextflow/22.10.6
 which nextflow
 ml dorado
 which dorado
-cd /scicomp/groups/OID/NCEZID/DFWED/EDLB/projects/the-chunnel/vasquez_samplesheet
-# module load guppy/6.4.6-gpu
-# which guppy_barcoder
+
+#Specify path to your DoradoBasecalling folder
+cd /scicomp/home-pure/suj7/EDLB/DoradoBasecalling
+#cd /scicomp/groups/OID/NCEZID/DFWED/EDLB/projects/the-chunnel/vasquez_samplesheet
 
 #This works!! (next 3 lines)
 #dorado download --model dna_r10.4.1_e8.2_400bps_sup@v4.2.0 
