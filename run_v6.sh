5c07b50aff3fcc5a06067b82ec9903776a914f00
vasquini <suj7@cdc.gov>
2024-05-13 09:49:50 -0400

    Example bash script to run whole pipeline (Guppy); cleaned up comments

--- run_v6.sh
+++ run_v6.sh
@@ -12,36 +12,22 @@ source /etc/profile
 #$ -l h_rt=24:00:00
 #$ -l h_vmem=200G
 # Specify where standard output and error are stored
-#$ -o test.out
-#$ -e test.err
+#$ -o piptest.out
+#$ -e piptest.err
 
 ml guppy
 which guppy_basecaller
 which guppy_barcoder
-ml nextflow/22.10.6
+ml nextflow
 which nextflow
 cd ~/EDLB
 module load guppy/6.4.6-gpu
 which guppy_barcoder
-#guppy_barcoder -i ~/DoradoRuns/fastq -s GXB014_Demux --device auto --compress_fastq  --barcode_kits "SQK-NBD114-24" --worker_threads 2
 
 nextflow main.nf --skip_illumina --skip_qc --samplesheet ~/gxb03287-22-014_samplesheet.csv \
  --medaka_model "r1041_e82_400bps_sup_g615" --fastq /scicomp/home-pure/suj7/EDLB/GXB014_Demux/fastq \
- --outdir TEST_GXB03287_22_014_Output --flye_args "--asm-coverage 50" -resume -work-dir ~/EDLB/work/cc/1d84961a222240bf6254e416c765a1/  
- #-resume 
- #-c /scicomp/reference/nextflow/configs/cdc-dev.config -profile rosalind,singularity
+ --outdir microPIPE_Output_20240510 --flye_args "--asm-coverage 50" 
 
-
-# nextflow main.nf --skip_illumina --samplesheet ~/gxb03287-22-013_samplesheet.csv \
-#  --medaka_model "r1041_e82_400bps_sup_g615" --fastq /scicomp/home-pure/suj7/EDLB/GXB013_Demux/fastq \
-#  --outdir GXB03287_22_013_Output --flye_args "--asm-coverage 50" --guppy_barcode_kits "SQK-NBD114-24"
-
-#guppy_barcoder -i ~/DoradoRuns/fastq -s GXB013_Demux --device auto --compress_fastq  --barcode_kits "SQK-NBD114-24" --worker_threads 2
-# nextflow main.nf --skip_illumina --demultiplexing --samplesheet ~/gxb03287-22-015_samplesheet.csv --guppy_config_gpu "dna_r10.4.1_e8.2_400bps_sup@v4.2.0.cfg" \
-#  --medaka_model "r1041_e82_400bps_sup_g615" --fastq /scicomp/home-pure/suj7/EDLB/GXB013_Demux/fastq \
-#  --outdir GPU_Test_OUTPUT --flye_args "--asm-coverage 50" --guppy_barcode_kits "SQK-NBD114-24"
-
-## I should break up gpu workflow from the cpu
 
 
 
